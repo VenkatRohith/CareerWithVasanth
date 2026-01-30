ce9dd337baff30a2a30aecde02585f3db7406869
Venkat Rohith Saripalli <venkatrohithcse63@gmail.com>
2026-01-30 19:01:31 +0530

    Fix spread operator pitfall by deep copying the details object

--- Assignments/Week-2/8 - spreadOperatorPitfall.js
+++ Assignments/Week-2/8 - spreadOperatorPitfall.js
@@ -24,5 +24,19 @@ The original state also changed as how complex data types are handled in javascr
 Objects, Arrays, Functions and other reference data types are not deep copied and only
 shallow copied and pointing to the same memory. Hence the output.
 
+This can be fixed by destructuring the details object as well, so that a new copy of details
+object
+
+Fix:
+const state = {
+  user: { id: 101, details: { city: 'Bangalore' } },
+  theme: 'dark'
+};
+
+const newState = {...state, details: {...state.details } };
+newState.user.details.city = 'Chennai';
+
+console.log(state.user.details.city); => 'Bangalore'
+
 Time taken to solve - 2min
 */
