0b5a015edfc42c70279e241a03f6bd7636d8dbd1
Venkat Rohith Saripalli <venkatrohithcse63@gmail.com>
2026-02-06 00:08:36 +0530

    Rename week-4 folder to week-3

--- src/routes/assignment-routes.js
+++ src/routes/assignment-routes.js
@@ -1,26 +1,26 @@
 import { lazy } from "react";
 
-const Week3Home = lazy(() => import("../assignments/week-4/index"));
+const Week3Home = lazy(() => import("../assignments/week-3"));
 const RealTimeUI = lazy(
-  () => import("../assignments/week-4/components/realtime-ui"),
+  () => import("../assignments/week-3/components/realtime-ui"),
 );
 const DOMInteraction = lazy(
-  () => import("../assignments/week-4/components/dom-interaction"),
+  () => import("../assignments/week-3/components/dom-interaction"),
 );
 const CustomHooks = lazy(
-  () => import("../assignments/week-4/components/custom-hooks"),
+  () => import("../assignments/week-3/components/custom-hooks"),
 );
 const StateManagement = lazy(
-  () => import("../assignments/week-4/components/state-management"),
+  () => import("../assignments/week-3/components/state-management"),
 );
 const CustomHooks2 = lazy(
-  () => import("../assignments/week-4/components/custom-hooks-2"),
+  () => import("../assignments/week-3/components/custom-hooks-2"),
 );
 const GeneralMachineCoding1 = lazy(
-  () => import("../assignments/week-4/components/general-machine-coding-1"),
+  () => import("../assignments/week-3/components/general-machine-coding-1"),
 );
 const GeneralMachineCoding2 = lazy(
-  () => import("../assignments/week-4/components/general-machine-coding-2"),
+  () => import("../assignments/week-3/components/general-machine-coding-2"),
 );
 
 // Export component references (not JSX) so this module contains plain JS values
